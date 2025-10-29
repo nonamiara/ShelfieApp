8ea3e249f8d0ea10237213f5e1f7dcda2958441a
nona <nonahitaly@gmail.com>
2025-10-29 17:57:08 +0400

    added themed parts

---
+++ app/_layout.jsx
@@ -0,0 +1,27 @@
+import { Stack } from "expo-router";
+import { Text, StyleSheet, View, useColorScheme } from "react-native";
+import { Colors } from "../constants/Colors";
+import { StatusBar } from "expo-status-bar";
+
+const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+  return (
+    <>
+    <StatusBar value="auto"/>
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="about" options={{ title: "About" }} />
+        <Stack.Screen name="contact" options={{ title: "Contact" }} />
+      </Stack>
+    </>
+  );
+};
+export default RootLayout;
+
+const styles = StyleSheet.create({});
---
+++ app/about.jsx
@@ -0,0 +1,35 @@
+import { Link } from "expo-router";
+import { Text, StyleSheet, View, useColorScheme } from "react-native";
+import { Colors } from "../constants/Colors";
+
+const About = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
+  return (
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <Text style={styles.title}> About Page </Text>
+      <Link href="/" style={styles.link}>
+        {" "}
+        Back Home
+      </Link>
+    </View>
+  );
+};
+export default About;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+});
