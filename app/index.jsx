d64e6e6a53d9e1f63ab5136f5df48047b6c5c9b8
nona <nonahitaly@gmail.com>
2025-10-30 13:08:19 +0400

    navigation bar

--- app/index.jsx
+++ app/index.jsx
@@ -31,6 +31,10 @@ const Home = () => {
         <ThemedText>Register Page</ThemedText>
       </Link>
 
+      <Link href="/profile" style={styles.link}>
+        <ThemedText>Profile Page</ThemedText>
+      </Link>
+
     </ThemedView>
   );
 };
