bd2fce906bfa6c331fd513ff4b682d0f0ec92370
nona <nonahitaly@gmail.com>
2025-11-06 16:54:23 +0400

    Real-Time Data

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { databases } from "../lib/appwrite";
+import { databases, client } from "../lib/appwrite";
 import { Databases, ID, Permission, Query, Role } from "react-native-appwrite";
 import { useUser } from "../hooks/useUser";
 
@@ -65,10 +65,22 @@ export function BooksProvider({ children }) {
   }
 
   useEffect(()=>{
+    let unsubscribe
+    const channel = `databases.${DATABASE_ID}.collections.${COLLECTION_ID}.documents`
     if (user){
       fetchBooks()
+      unsubscribe = client.subscribe(channel, (response)=> {
+          const { payload, events }=response
+
+          if(events[0].includes(`create`)){
+            setBooks((prevBooks)=>[...prevBooks, payload])
+          }
+      })
     } else {
       setBooks([])
+    }
+    return ()=>{
+      if(unsubscribe) unsubscribe()
     }
   }, [user])
 
